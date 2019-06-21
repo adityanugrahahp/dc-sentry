08f79bd7fd11b07f7a98848a5d84d77a2b3d75e4
Dimas <dimas.wicaksono@pelni.co.id>
2019-06-21 10:50:55 +0700

    fix login error

--- application/controllers/Login.php
+++ application/controllers/Login.php
@@ -7,45 +7,40 @@ class Login extends CI_Controller {
 		$this->load->view('layouts/V_login');
 		
 	}
+
 	public function login_action()
 	{
 		$username = $this->input->post('username');
 		$password = $this->input->post('password');
 		$this->load->library('form_validation');
-                $this->load->model('M_visitor');
+        
+		$this->load->model('M_visitor');
+
 		$this->form_validation->set_rules('username', 'username', 'required');
 		$this->form_validation->set_rules('password', 'password', 'required');
-		if ($this->form_validation->run() != FALSE)
-                {
-                	$password = md5($password);
-                	$result_login = $this->M_visitor->login($username,$password);
-                	if($result_login['status']=='success')
-                	{
-                		
-                        $data = $result_login['data'];
-                        $_SESSION['userID'] = $data['id_user'];
-                		$_SESSION['userName'] = $data['username'];
-                		$_SESSION['locationID'] = $data['location_id'];
-                		$_SESSION['loket_name'] = $data['loket_name'];
-                                $_SESSION['role'] = $data['role'];
-                		if($data['role']==1)
-                		{
-                			redirect('/');
-                		}
-                		
-                	}
-                	else
-                	{
-                		$this->session->set_flashdata('message', $result_login['status_message']);
-                		redirect('Login');
 
-                	}
-                }
-                else
-                {
-                	$this->session->set_flashdata('message', 'username dan password tidak boleh kosong');
-                	redirect('Login');
-                }
+		if ($this->form_validation->run() != FALSE){
+			$password 		= md5($password);
+			$result_login 	= $this->M_visitor->login($username,$password);
+			
+			if($result_login['status']=='success'){
+				$data = $result_login['data'];
+
+				$_SESSION['userID'] 	= $data['id_user'];
+				$_SESSION['userName'] 	= $data['username'];
+				$_SESSION['locationID'] = $data['location_id'];
+				$_SESSION['loket_name'] = $data['loket_name'];
+				$_SESSION['role'] 		= $data['role'];
+
+				redirect('/');
+			}else{
+				$this->session->set_flashdata('message', $result_login['status_message']);
+				redirect('Login');
+			}
+		}else{
+			$this->session->set_flashdata('message', 'username dan password tidak boleh kosong');
+			redirect('Login');
+		}
 	}
 
 	public function logout()
