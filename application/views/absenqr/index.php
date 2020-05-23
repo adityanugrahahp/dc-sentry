3336afefeaf19e162a18382d157253a00668a8b0
Bagus Navyan Putra <vyan@Baguss-MacBook-Pro.local>
2020-05-23 15:26:05 +0700

    Tambah Fitur Absen QR

---
+++ application/views/absenqr/index.php
@@ -0,0 +1,231 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<title>Login V18</title>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+<!--===============================================================================================-->	
+	<link rel="icon" type="image/png" href="<?= base_url(''); ?>assets/images/icons/favicon.ico"/>
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="<?= base_url(''); ?>assets/vendor/bootstrap/css/bootstrap.min.css">
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="<?= base_url(''); ?>assets/fonts/font-awesome-4.7.0/css/font-awesome.min.css">
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="<?= base_url(''); ?>assets/fonts/Linearicons-Free-v1.0.0/icon-font.min.css">
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="<?= base_url(''); ?>assets/vendor/animate/animate.css">
+<!--===============================================================================================-->	
+	<link rel="stylesheet" type="text/css" href="<?= base_url(''); ?>assets/vendor/css-hamburgers/hamburgers.min.css">
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="<?= base_url(''); ?>assets/vendor/animsition/css/animsition.min.css">
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="<?= base_url(''); ?>assets/vendor/select2/select2.min.css">
+<!--===============================================================================================-->	
+	<link rel="stylesheet" type="text/css" href="<?= base_url(''); ?>assets/vendor/daterangepicker/daterangepicker.css">
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="<?= base_url(''); ?>assets/css/util.css">
+	<link rel="stylesheet" type="text/css" href="<?= base_url(''); ?>assets/css/main.css">
+<!--===============================================================================================-->
+
+</head>
+<body style="background-color: #666666;">
+	
+	<div class="limiter">
+		<div class="container-login100">
+			<div class="wrap-login100">
+				<form class="login100-form validate-form">
+					<p><center>
+						<span><h4 id="date"></h4><br></span>
+					</center></p>
+					<span class="login100-form-title p-b-25">
+						<h3>Absent User Reviews</h3>
+					</span>
+					
+					<div class="text-center">
+						<table class="table fixed_header">
+							<thead>
+								<tr>
+									<th scope="col">#</th>
+					                <th scope="col">Image</th>
+					                <th scope="col">Nama Lengkap</th>
+					                <th scope="col"><center>NRP</center></th>
+					                <th scope="col">Time</th>
+								</tr>
+							</thead>
+							<tbody>
+				                <tr>
+				                  <th scope="row">1</th>
+				                  <td><img src="<?= base_url(''); ?>assets/images/user.png" alt="Image" width="70px"></td>
+				                  <td><small><b>Bagus Navyan Putra</b></small><br><small>(Manager Pengembangan Teknologi Informasi)</small></td>
+				                  <td>xxxxxxx</td>
+				                  <td>07.00</td>
+				                </tr>
+				                <tr>
+				                  <th scope="row">2</th>
+				                  <td><img src="<?= base_url(''); ?>assets/images/user.png" alt="Image" width="70px"></td>
+				                  <td><small><b>Bagus Navyan Putra</b></small><br><small>(Manager Pengembangan Teknologi Informasi)</small></td>
+				                  <td>xxxxxxx</td>
+				                  <td>07.00</td>
+				                </tr>
+				                <tr>
+				                  <th scope="row">3</th>
+				                  <td><img src="<?= base_url(''); ?>assets/images/user.png" alt="Image" width="70px"></td>
+				                  <td><small><b>Bagus Navyan Putra</b></small><br><small>(Manager Pengembangan Teknologi Informasi)</small></td>
+				                  <td>xxxxxxx</td>
+				                  <td>07.00</td>
+				                </tr>
+				                <tr>
+				                  <th scope="row">4</th>
+				                  <td><img src="<?= base_url(''); ?>assets/images/user.png" alt="Image" width="70px"></td>
+				                  <td><small><b>Bagus Navyan Putra</b></small><br><small>(Manager Pengembangan Teknologi Informasi)</small></td>
+				                  <td>xxxxxxx</td>
+				                  <td>07.00</td>
+				                </tr>
+				                <tr>
+				                  <th scope="row">5</th>
+				                  <td><img src="<?= base_url(''); ?>assets/images/user.png" alt="Image" width="70px"></td>
+				                  <td><small><b>Bagus Navyan Putra</b></small><br><small>(Manager Pengembangan Teknologi Informasi)</small></td>
+				                  <td>xxxxxxx</td>
+				                  <td>07.00</td>
+				                </tr>
+				                <tr>
+				                  <th scope="row">6</th>
+				                  <td><img src="<?= base_url(''); ?>assets/images/user.png" alt="Image" width="70px"></td>
+				                  <td><small><b>Bagus Navyan Putra</b></small><br><small>(Manager Pengembangan Teknologi Informasi)</small></td>
+				                  <td>xxxxxxx</td>
+				                  <td>07.00</td>
+				                </tr>
+				                <tr>
+				                  <th scope="row">7</th>
+				                  <td><img src="<?= base_url(''); ?>assets/images/user.png" alt="Image" width="70px"></td>
+				                  <td><small><b>Bagus Navyan Putra</b></small><br><small>(Manager Pengembangan Teknologi Informasi)</small></td>
+				                  <td>xxxxxxx</td>
+				                  <td>07.00</td>
+				                </tr>
+				                <tr>
+				                  <th scope="row">8</th>
+				                  <td><img src="<?= base_url(''); ?>assets/images/user.png" alt="Image" width="70px"></td>
+				                  <td><small><b>Bagus Navyan Putra</b></small><br><small>(Manager Pengembangan Teknologi Informasi)</small></td>
+				                  <td>xxxxxxx</td>
+				                  <td>07.00</td>
+				                </tr>
+				                <tr>
+				                  <th scope="row">9</th>
+				                  <td><img src="<?= base_url(''); ?>assets/images/user.png" alt="Image" width="70px"></td>
+				                  <td><small><b>Bagus Navyan Putra</b></small><br><small>(Manager Pengembangan Teknologi Informasi)</small></td>
+				                  <td>xxxxxxx</td>
+				                  <td>07.00</td>
+				                </tr>
+				                <tr>
+				                  <th scope="row">10</th>
+				                  <td><img src="<?= base_url(''); ?>assets/images/user.png" alt="Image" width="70px"></td>
+				                  <td><small><b>Bagus Navyan Putra</b></small><br><small>(Manager Pengembangan Teknologi Informasi)</small></td>
+				                  <td>xxxxxxx</td>
+				                  <td>07.00</td>
+				                </tr>
+				              </tbody>
+						</table>
+					</div>
+				</form>
+
+				<div class="login100-more" style="background-image: url('<?= base_url(''); ?>assets/images/5.jpg');">
+					<div class="row">&nbsp;</div>
+					<span class="login100-form-title p-b-25">
+						<h1><b style="color: rgba(255, 255, 255, 0.8); ">
+							PT.Pelayaran Nasional indonesia
+						</b></h1>
+					</span>
+					<div class="row">
+						<h5 class="col align-self-center">
+							<b style="color: rgba(255, 255, 255, 0.8); ">
+								<center><h1 id="time"></h1></center>
+							</b>
+						</h5></div>
+					<div class="row">&nbsp;</div>
+					<div class="row">
+						<h5 class="col align-self-center">
+							<b style="color: rgba(255, 255, 255, 0.8); ">
+								<center>(SCAN DISINI UNTUK ABSENSI)</center><br>
+							</b>
+						</h5></div>
+					<div class="row">
+						<div class="col align-self-center">
+							<center><img src="<?= base_url(''); ?>assets/images/qr-code.PNG" alt="Image" class="img-fluid" width="55%"></center></div>
+						</div>
+					</div>
+					
+					
+				</div>
+			</div>
+		</div>
+	</div>
+
+<script>
+		var months = ['Januari','Februari','Maret','April','Mei','Juni','Juli','Agustus','September','Oktober','November','Desember'];
+		var days = ['Senin','Selasa','Rabu','Kamis','Jumat','Sabtu','Minggu'];
+		
+		var date = new Date();
+		var n = date.getDay();
+		var day = date.getDate();
+		var month = date.getMonth();
+		var year = <?php echo date('Y')?>
+
+		document.getElementById("date").innerHTML=days[n-1]+", "+" "+day+" "+months[month]+" "+year;
+	</script>
+	
+<script type="text/javascript">
+		function showTime(){
+			var a_p = "";
+			var today = new Date();
+			var curr_hour = today.getHours();
+			var curr_minute = today.getMinutes();
+			var curr_second = today.getSeconds();
+
+			// if (curr_hour<12) {
+			// 	a_p = "AM";
+			// }else {
+			// 	a_p = "PM";
+			// }
+
+			// if (curr_hour == 0) {
+			// 	curr_hour=12;
+			// }
+			// if (curr_hour == 12) {
+			// 	curr_hour=curr_hour-12;
+			// }
+			curr_hour = checkTime(curr_hour);
+			curr_minute = checkTime(curr_minute);
+			curr_second = checkTime(curr_second);
+
+			document.getElementById('time').innerHTML= curr_hour+":"+curr_minute+":"+curr_second+" "+a_p;
+		}
+
+		function checkTime(i){
+			if (i<10) {
+				i = "0"+i;
+			}
+			return i;
+		}
+		setInterval(showTime,500);
+	</script>
+	
+	
+<!--===============================================================================================-->
+	<script src="<?= base_url(''); ?>assets/vendor/jquery/jquery-3.2.1.min.js"></script>
+<!--===============================================================================================-->
+	<script src="<?= base_url(''); ?>assets/vendor/animsition/js/animsition.min.js"></script>
+<!--===============================================================================================-->
+	<script src="<?= base_url(''); ?>assets/vendor/bootstrap/js/popper.js"></script>
+	<script src="<?= base_url(''); ?>assets/vendor/bootstrap/js/bootstrap.min.js"></script>
+<!--===============================================================================================-->
+	<script src="<?= base_url(''); ?>assets/vendor/select2/select2.min.js"></script>
+<!--===============================================================================================-->
+	<script src="<?= base_url(''); ?>assets/vendor/daterangepicker/moment.min.js"></script>
+	<script src="<?= base_url(''); ?>assets/vendor/daterangepicker/daterangepicker.js"></script>
+<!--===============================================================================================-->
+	<script src="<?= base_url(''); ?>assets/vendor/countdowntime/countdowntime.js"></script>
+<!--===============================================================================================-->
+	<script src="<?= base_url(''); ?>assets/js/main.js"></script>
+
+</body>
+</html>
