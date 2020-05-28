243799024685a1d883bf05582899cd8b515a6420
wahyudewantoro <wahyudewantoro28@gmail.com>
2020-05-28 13:12:56 +0700

    Update V_screen

--- application/views/absenqr/V_screen.php
+++ application/views/absenqr/V_screen.php
@@ -16,101 +16,111 @@
 	<link rel="stylesheet" type="text/css" href="<?= base_url(THEME_PATH); ?>vendor/daterangepicker/daterangepicker.css">
 	<link rel="stylesheet" type="text/css" href="<?= base_url(THEME_PATH); ?>css/util.css">
 	<link rel="stylesheet" type="text/css" href="<?= base_url(THEME_PATH); ?>css/main.css">
+	<style type="text/css">
+		.login100-more {
+  			width: calc(100% - 460px);
+  		}
+  		.login100-form {
+  			width: 460px;
+  			background-color: #fff;
+  		}
+
+	</style>
 </head>
-<body style="background-color: #666666;">
-	
+<body style="background-color: #666666;">	
 	<div class="limiter">
 		<div class="container-login100">
 			<div class="wrap-login100">
 				<form class="login100-form validate-form">
 					<p><center>
-						<span><h4 id="date"></h4><br></span>
+						<img src="<?= base_url(THEME_PATH); ?>images/logo_head.png" alt="Image" width="70px">
+						<h4>PT.Pelayaran Nasional Indonesia (Persero)</h4>
 					</center></p>
 					<span class="login100-form-title p-b-25">
-						<h3>Absent User Reviews</h3>
+						<h3><b>Absensi Pegawai</b></h3>
 					</span>
 					
 					<div class="text-center">
-						<table class="table table-condensed fixed_header">
+						<table class="table-condensed fixed_header">
 							<thead>
 								<tr>
-									<th scope="col">#</th>
-					                <th scope="col">Image</th>
-					                <th scope="col">Nama Lengkap</th>
-					                <th scope="col"><center>NRP</center></th>
-					                <th scope="col">Time</th>
+									<th width="50px" scope="col">#</th>
+					                <!-- <th scope="col">Image</th> -->
+					                <th width="200px" scope="col">Nama Lengkap</th>
+					                <th width="200px" scope="col"><center>NRP</center></th>
+					                <th width="200px" scope="col">Time</th>
 								</tr>
 							</thead>
 							<tbody>
 				                <tr>
-				                  <th scope="row">1</th>
-				                  <td><img src="<?= base_url(THEME_PATH); ?>images/user.png" alt="Image" width="70px"></td>
-				                  <td><small><b>Bagus Navyan Putra</b></small><br><small>(Manager Pengembangan Teknologi Informasi)</small></td>
-				                  <td>xxxxxxx</td>
-				                  <td>07.00</td>
+				                  <th width="50px" scope="row">1</th>
+				                  <!-- <td><img src="<?= base_url(THEME_PATH); ?>images/user.png" alt="Image" width="70px"></td> -->
+				                  <td width="200px"><small><b>Bagus Navyan Putra</b></small><br><small>(Manager Pengembangan Teknologi Informasi)</small></td>
+				                  <td width="200px">xxxxxxx</td>
+				                  <td width="200px">07.00</td>
 				                </tr>
 				                <tr>
 				                  <th scope="row">2</th>
-				                  <td><img src="<?= base_url(THEME_PATH); ?>images/user.png" alt="Image" width="70px"></td>
-				                  <td><small><b>Bagus Navyan Putra</b></small><br><small>(Manager Pengembangan Teknologi Informasi)</small></td>
-				                  <td>xxxxxxx</td>
-				                  <td>07.00</td>
+				                  <!-- <td><img src="<?= base_url(THEME_PATH); ?>images/user.png" alt="Image" width="70px"></td> -->
+				                  <td width="200px"><small><b>Bagus Navyan Putra</b></small><br><small>(Manager Pengembangan Teknologi Informasi)</small></td>
+				                  <td width="200px">xxxxxxx</td>
+				                  <td width="200px">07.00</td>
 				                </tr>
 				                <tr>
 				                  <th scope="row">3</th>
-				                  <td><img src="<?= base_url(THEME_PATH); ?>images/user.png" alt="Image" width="70px"></td>
-				                  <td><small><b>Bagus Navyan Putra</b></small><br><small>(Manager Pengembangan Teknologi Informasi)</small></td>
-				                  <td>xxxxxxx</td>
-				                  <td>07.00</td>
+				                  <!-- <td><img src="<?= base_url(THEME_PATH); ?>images/user.png" alt="Image" width="70px"></td> -->
+				                  <td width="200px"><small><b>Bagus Navyan Putra</b></small><br><small>(Manager Pengembangan Teknologi Informasi)</small></td>
+				                  <td width="200px">xxxxxxx</td>
+				                  <td width="200px">07.00</td>
 				                </tr>
 				                <tr>
 				                  <th scope="row">4</th>
-				                  <td><img src="<?= base_url(THEME_PATH); ?>images/user.png" alt="Image" width="70px"></td>
-				                  <td><small><b>Bagus Navyan Putra</b></small><br><small>(Manager Pengembangan Teknologi Informasi)</small></td>
-				                  <td>xxxxxxx</td>
-				                  <td>07.00</td>
+				                  <!-- <td><img src="<?= base_url(THEME_PATH); ?>images/user.png" alt="Image" width="70px"></td> -->
+				                  <td width="200px"><small><b>Bagus Navyan Putra</b></small><br><small>(Manager Pengembangan Teknologi Informasi)</small></td>
+				                  <td width="200px">xxxxxxx</td>
+				                  <td width="200px">07.00</td>
 				                </tr>
 				                <tr>
 				                  <th scope="row">5</th>
-				                  <td><img src="<?= base_url(THEME_PATH); ?>images/user.png" alt="Image" width="70px"></td>
-				                  <td><small><b>Bagus Navyan Putra</b></small><br><small>(Manager Pengembangan Teknologi Informasi)</small></td>
-				                  <td>xxxxxxx</td>
-				                  <td>07.00</td>
+				                  <!-- <td><img src="<?= base_url(THEME_PATH); ?>images/user.png" alt="Image" width="70px"></td> -->
+				                  <td width="200px"><small><b>Bagus Navyan Putra</b></small><br><small>(Manager Pengembangan Teknologi Informasi)</small></td>
+				                  <td width="200px">xxxxxxx</td>
+				                  <td width="200px">07.00</td>
 				                </tr>
 				                <tr>
 				                  <th scope="row">6</th>
-				                  <td><img src="<?= base_url(THEME_PATH); ?>images/user.png" alt="Image" width="70px"></td>
-				                  <td><small><b>Bagus Navyan Putra</b></small><br><small>(Manager Pengembangan Teknologi Informasi)</small></td>
-				                  <td>xxxxxxx</td>
-				                  <td>07.00</td>
+				                  <!-- <td><img src="<?= base_url(THEME_PATH); ?>images/user.png" alt="Image" width="70px"></td> -->
+				                  <td width="200px"><small><b>Bagus Navyan Putra</b></small><br><small>(Manager Pengembangan Teknologi Informasi)</small></td>
+				                  <td width="200px">xxxxxxx</td>
+				                  <td width="200px">07.00</td>
 				                </tr>
 				                <tr>
 				                  <th scope="row">7</th>
-				                  <td><img src="<?= base_url(THEME_PATH); ?>images/user.png" alt="Image" width="70px"></td>
-				                  <td><small><b>Bagus Navyan Putra</b></small><br><small>(Manager Pengembangan Teknologi Informasi)</small></td>
-				                  <td>xxxxxxx</td>
-				                  <td>07.00</td>
+				                  <!-- <td><img src="<?= base_url(THEME_PATH); ?>images/user.png" alt="Image" width="70px"></td> -->
+				                  <td width="200px"><small><b>Bagus Navyan Putra</b></small><br><small>(Manager Pengembangan Teknologi Informasi)</small></td>
+				                  <td width="200px">xxxxxxx</td>
+				                  <td width="200px">07.00</td>
 				                </tr>
 				                <tr>
 				                  <th scope="row">8</th>
-				                  <td><img src="<?= base_url(THEME_PATH); ?>images/user.png" alt="Image" width="70px"></td>
-				                  <td><small><b>Bagus Navyan Putra</b></small><br><small>(Manager Pengembangan Teknologi Informasi)</small></td>
-				                  <td>xxxxxxx</td>
-				                  <td>07.00</td>
+				                  <!-- <td><img src="<?= base_url(THEME_PATH); ?>images/user.png" alt="Image" width="70px"></td> -->
+				                  <td width="200px"><small><b>Bagus Navyan Putra</b></small><br><small>(Manager Pengembangan Teknologi Informasi)</small></td>
+				                  <td width="200px">xxxxxxx</td>
+				                  <td width="200px">07.00</td>
 				                </tr>
 				                <tr>
 				                  <th scope="row">9</th>
-				                  <td><img src="<?= base_url(THEME_PATH); ?>images/user.png" alt="Image" width="70px"></td>
-				                  <td><small><b>Bagus Navyan Putra</b></small><br><small>(Manager Pengembangan Teknologi Informasi)</small></td>
-				                  <td>xxxxxxx</td>
-				                  <td>07.00</td>
+				                  <!-- <td><img src="<?= base_url(THEME_PATH); ?>images/user.png" alt="Image" width="70px"></td> -->
+				                  <td width="200px"><small><b>Bagus Navyan Putra</b></small><br><small>(Manager Pengembangan Teknologi Informasi)</small></td>
+				                  <td width="200px">xxxxxxx</td>
+				                  <td width="200px">07.00</td>
 				                </tr>
 				                <tr>
 				                  <th scope="row">10</th>
-				                  <td><img src="<?= base_url(THEME_PATH); ?>images/user.png" alt="Image" width="70px"></td>
-				                  <td><small><b>Bagus Navyan Putra</b></small><br><small>(Manager Pengembangan Teknologi Informasi)</small></td>
-				                  <td>xxxxxxx</td>
-				                  <td>07.00</td>
+				                  <!-- <td><img src="<?= base_url(THEME_PATH); ?>images/user.png" alt="Image" width="70px"></td> -->
+				                  <td width="200px"><small><b>Bagus Navyan Putra</b></small><br><small>(Manager Pengembangan Teknologi Informasi)</small></td>
+				                  <td width="200px">xxxxxxx</td>
+				                  <td width="200px">07.00</td>
 				                </tr>
 				              </tbody>
 						</table>
@@ -121,16 +131,18 @@
 					<div class="row">&nbsp;</div>
 					<span class="login100-form-title p-b-25">
 						<h1><b style="color: rgba(255, 255, 255, 0.8); ">
-							PT.Pelayaran Nasional indonesia
+							#DISPLAY NAME#							
 						</b></h1>
-					</span>
-					<div class="row">
+						<h4 style="color: rgba(255, 255, 255, 0.8); " id="date"></h4>
 						<h5 class="col align-self-center">
 							<b style="color: rgba(255, 255, 255, 0.8); ">
 								<center><h1 id="time"></h1></center>
 							</b>
-						</h5></div>
-					<div class="row">&nbsp;</div>
+						</h5>
+					</span>
+					<div class="row">
+						</div>
+					<!-- <div class="row">&nbsp;</div> -->
 					<div class="row">
 						<h5 class="col align-self-center">
 							<b style="color: rgba(255, 255, 255, 0.8); ">
@@ -139,65 +151,17 @@
 						</h5></div>
 					<div class="row">
 						<div class="col align-self-center">
-							<center><img src="<?= base_url(THEME_PATH); ?>images/qr-code.png" alt="Image" class="img-fluid" width="55%"></center></div>
+							<center><img src="<?= base_url(THEME_PATH); ?>images/qr-code.png" alt="Image" class="img-fluid" width="50%"></center></div>
 						</div>
+
 					</div>
-					
 					
 				</div>
 			</div>
 		</div>
 	</div>
 
-<script>
-		var months = ['Januari','Februari','Maret','April','Mei','Juni','Juli','Agustus','September','Oktober','November','Desember'];
-		var days = ['Senin','Selasa','Rabu','Kamis','Jumat','Sabtu','Minggu'];
-		
-		var date = new Date();
-		var n = date.getDay();
-		var day = date.getDate();
-		var month = date.getMonth();
-		var year = <?php echo date('Y')?>
-
-		document.getElementById("date").innerHTML=days[n-1]+", "+" "+day+" "+months[month]+" "+year;
-	</script>
-	
-<script type="text/javascript">
-		function showTime(){
-			var a_p = "";
-			var today = new Date();
-			var curr_hour = today.getHours();
-			var curr_minute = today.getMinutes();
-			var curr_second = today.getSeconds();
-
-			// if (curr_hour<12) {
-			// 	a_p = "AM";
-			// }else {
-			// 	a_p = "PM";
-			// }
-
-			// if (curr_hour == 0) {
-			// 	curr_hour=12;
-			// }
-			// if (curr_hour == 12) {
-			// 	curr_hour=curr_hour-12;
-			// }
-			curr_hour = checkTime(curr_hour);
-			curr_minute = checkTime(curr_minute);
-			curr_second = checkTime(curr_second);
-
-			document.getElementById('time').innerHTML= curr_hour+":"+curr_minute+":"+curr_second+" "+a_p;
-		}
-
-		function checkTime(i){
-			if (i<10) {
-				i = "0"+i;
-			}
-			return i;
-		}
-		setInterval(showTime,500);
-	</script>
-	
+	<script src="<?= base_url(THEME_PATH); ?>dist/js/pages/absenqr/screen.js"></script>
 	<script src="<?= base_url(THEME_PATH); ?>vendor/jquery/jquery-3.2.1.min.js"></script>
 	<script src="<?= base_url(THEME_PATH); ?>vendor/animsition/js/animsition.min.js"></script>
 	<script src="<?= base_url(THEME_PATH); ?>vendor/bootstrap/js/popper.js"></script>
--- assets/dist/js/pages/absenqr/screen.js
+++ assets/dist/js/pages/absenqr/screen.js
@@ -1,3 +1,48 @@
-$(document).ready(function () {
-	
-});
+var months = ['Januari','Februari','Maret','April','Mei','Juni','Juli','Agustus','September','Oktober','November','Desember'];
+		var days = ['Senin','Selasa','Rabu','Kamis','Jumat','Sabtu','Minggu'];
+		
+		var date = new Date();
+		var n = date.getDay();
+		var day = date.getDate();
+		var month = date.getMonth();
+		var year = new Date().getFullYear();
+
+// $(document).ready(function () {
+	document.getElementById("date").innerHTML=days[n-1]+", "+" "+day+" "+months[month]+" "+year;	
+// });
+
+
+
+function showTime(){
+			var a_p = "";
+			var today = new Date();
+			var curr_hour = today.getHours();
+			var curr_minute = today.getMinutes();
+			var curr_second = today.getSeconds();
+
+			// if (curr_hour<12) {
+			// 	a_p = "AM";
+			// }else {
+			// 	a_p = "PM";
+			// }
+
+			// if (curr_hour == 0) {
+			// 	curr_hour=12;
+			// }
+			// if (curr_hour == 12) {
+			// 	curr_hour=curr_hour-12;
+			// }
+			curr_hour = checkTime(curr_hour);
+			curr_minute = checkTime(curr_minute);
+			curr_second = checkTime(curr_second);
+
+			document.getElementById('time').innerHTML= curr_hour+":"+curr_minute+":"+curr_second+" "+a_p;
+		}
+
+function checkTime(i){
+		if (i<10) {
+			i = "0"+i;
+		}
+		return i;
+		}
+setInterval(showTime,500);
