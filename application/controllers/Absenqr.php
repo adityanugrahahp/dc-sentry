5c13f27bb693b92e59a64548b1e0e497dbad579d
Dimas Wicaksono <dimas.wicaksono@pelni.co.id>
2020-06-03 00:27:25 +0700

    tes redis

--- application/controllers/Absenqr.php
+++ application/controllers/Absenqr.php
@@ -11,7 +11,7 @@ class Absenqr extends MY_Controller {
 		$this->load->library(['user_agent', 'table']);
 
 		// load driver for caching
-        $this->load->driver('cache', ['adapter' => 'redis', 'key_prefix' => 'absenqr_']);
+        $this->load->driver('cache', ['adapter' => 'redis']);
 
 		// load models
 		$this->load->model('M_absenqr');
