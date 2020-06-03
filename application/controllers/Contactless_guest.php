34bbdc430150127cf2fd4b286605c49327090710
wahyudewantoro <wahyudewantoro28@gmail.com>
2020-06-03 20:26:45 +0700

    Update View Pendaftaran Visitor/Tamu

--- application/controllers/Contactless_guest.php
+++ application/controllers/Contactless_guest.php
@@ -3,6 +3,14 @@
 class Contactless_guest extends MY_Controller {
 
 	public function index(){
-		$this->load->view('contactless_guest/index');
+		$opt = [];
+		// daftar nama lantai
+		for($i = 1;$i <= 11; $i++){
+			$opt['Lantai '.$i] = 'Lantai '.$i;
+		}
+
+			$data['tujuan'] 	= $opt;
+
+		$this->load->view('contactless_guest/index', $data);
 	}
 }
