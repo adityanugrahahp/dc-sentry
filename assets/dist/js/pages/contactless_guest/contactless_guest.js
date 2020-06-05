8fd701b6f430b030f66977faaf012ea11ac48ae4
Wahyu Dewantoro <wahyudewantoro28@gmail.com>
2020-06-05 12:33:01 +0700

    Update contactless_guest.js

--- assets/dist/js/pages/contactless_guest/contactless_guest.js
+++ assets/dist/js/pages/contactless_guest/contactless_guest.js
@@ -8,11 +8,12 @@ $(document).ready(function(){
         	url: action,
             data: data,
             method: method,
-            // beforeSend: function() {
+            beforeSend: function() {
 
-            // },
+            },
             success: function(data) {
-                $("#notice").fadeIn(400);
+                $("#myAlert").show();
+                $('#formhide').hide();
             }
         })
 	})
