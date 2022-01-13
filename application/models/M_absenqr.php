ee10faec8ac610938194f84ace47012ca6c57e64
Dimas Wicaksono <dimas.wicaksono@pelni.co.id>
2022-01-13 15:46:27 +0700

    fixed error

--- application/models/M_absenqr.php
+++ application/models/M_absenqr.php
@@ -52,9 +52,9 @@ class M_absenqr extends MY_Model {
 		}
     
     if($cab_st_aktif == 'Y'){
-      $this->db->or_where('cab_st_kp', 'Y');
+      $this->db->where('cab_st_kp', 'Y');
     }
-    
+
 		$this->db->order_by('cab_sdm_klass ASC, cab_kode ASC');
 
 		$q = $this->db->get('cabang');
