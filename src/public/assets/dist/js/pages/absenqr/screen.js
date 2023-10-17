971fa72a3b3eb4b17fbee9620761bad4ad999d7d
Dimas Wicaksono <dimas.wicaksono@pelni.co.id>
2023-10-17 09:23:44 +0700

    Disabled location

--- src/public/assets/dist/js/pages/absenqr/screen.js
+++ src/public/assets/dist/js/pages/absenqr/screen.js
@@ -9,8 +9,8 @@ var module_url      = base_url + 'absenqr';
 var next_update     = null;
 var months          = ['Januari','Februari','Maret','April','Mei','Juni','Juli','Agustus','September','Oktober','November','Desember'];
 var days            = ['Minggu','Senin','Selasa','Rabu','Kamis','Jumat','Sabtu'];
-var display_lat     = 0;
-var display_lon     = 0;
+var display_lat     = 10;
+var display_lon     = 10;
 
 const refreshQRInterval         = 1000; // 1 detik
 const checkNewScanInterval      = 3000; // 3 detik
@@ -153,14 +153,14 @@ function _getGeoLocationDisplay(){
     } catch (error) {
       alert(`ERROR: ${error.message}`);
       
-      display_lat = 1;
-      display_lon = 1;
+      display_lat = 10;
+      display_lon = 10;
     }
   } else { 
     alert("Geolocation is not supported by this browser.");
 
-    display_lat = 1;
-    display_lon = 1;
+    display_lat = 10;
+    display_lon = 10;
   }
 }
 
