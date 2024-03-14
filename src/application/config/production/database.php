278de45f2ea15b25ea071ce2f5d805770baeea95
Dimas Wicaksono <dimas.wicaksono@pelni.co.id>
2024-03-14 13:59:26 +0700

    Updated

--- src/application/config/production/database.php
+++ src/application/config/production/database.php
@@ -5,7 +5,7 @@ $query_builder = TRUE;
 
 $db['default'] = array(
 	'dsn'	=> '',
-	'hostname' => '172.16.101.99',
+	'hostname' => '10.1.0.110',
 	'username' => 'dashboard',
 	'password' => 's1mp3ln1',
 	'database' => 'dashboard',
