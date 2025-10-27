0c13341e37afb6dadcfc5e32ec5cc87468b705d6
Dimas Wicaksono <dimas.wicaksono@pelni.co.id>
2025-10-27 08:51:14 +0700

    Updated redis

--- src/application/config/redis.php
+++ src/application/config/redis.php
@@ -2,7 +2,7 @@
 
 $config['socket_type']  = 'tcp'; //`tcp` or `unix`
 $config['socket']       = ''; // in case of `unix` socket type
-$config['host']         = '172.16.253.53';
-$config['password']     = NULL;
+$config['host']         = '172.16.102.51';
+$config['password']     = 'redisp3ln1';
 $config['port']         = 6379;
 $config['timeout']      = 0;
