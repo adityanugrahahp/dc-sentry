864a3604c35567780e4f7ef851f0ff402fad1dfe
adityanugrahahp <aditking2307@gmail.com>
2025-11-24 14:00:14 +0700

    update config

--- src/application/config/config.php
+++ src/application/config/config.php
@@ -1,71 +1,54 @@
-<?php defined('BASEPATH') or exit('No direct script access allowed');
+<?php
+// ENABLE ERROR DISPLAY - TAMBAHKAN DI BARIS PALING ATAS
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
 
-$config['base_url']                 = isset($_SERVER['SERVICE_PORT']) && $_SERVER['SERVICE_PORT'] == 'https' ? 'https:' : 'http:';
-$config['base_url']                 .= "//" . $_SERVER['HTTP_HOST'];
-$config['base_url']                 .= str_replace(basename($_SERVER['SCRIPT_NAME']), "", $_SERVER['SCRIPT_NAME']);
+defined('BASEPATH') or exit('No direct script access allowed');
 
-$config['index_page']                       = '';
-$config['uri_protocol']                      = 'REQUEST_URI';
-$config['url_suffix']                       = '';
-$config['language']                            = 'english';
-$config['charset']                             = 'UTF-8';
-$config['enable_hooks']                   = TRUE;
-$config['subclass_prefix']                 = 'MY_';
-$config['composer_autoload']             = '../vendor/autoload.php';
-$config['permitted_uri_chars']           = 'a-z 0-9~%.:_\-';
-$config['enable_query_strings']       = FALSE;
-$config['controller_trigger']            = 'c';
-$config['function_trigger']             = 'm';
-$config['directory_trigger']             = 'd';
-$config['allow_get_array']                 = TRUE;
-$config['log_threshold']                   = 1;
-$config['log_path']                         = '';
-$config['log_file_extension']           = '.log';
-$config['log_file_permissions']       = 0644;
-$config['log_date_format']                 = 'Y-m-d H:i:s';
-$config['error_views_path']             = '';
-$config['cache_path']                       = '';
-$config['cache_query_string']           = FALSE;
+$config['base_url'] = 'https://visitor-dacen.pelni.co.id/';
 
-$config['encryption_key']                 = 'wpAc3HuWyJ50BLK0YqeQ4X9j7tOkIC8Nla7J4xVG';
-$config['sess_driver']                       = 'files';
-$config['sess_cookie_name']             = 'pelni_visitor';
-$config['sess_expiration']                 = 7200;
-$config['sess_save_path']                 = '/tmp';
-$config['sess_match_ip']                   = FALSE;
-$config['sess_time_to_update']           = 300;
-$config['sess_regenerate_destroy']     = TRUE;
+$config['index_page'] = '';
+$config['uri_protocol'] = 'REQUEST_URI';
+$config['url_suffix'] = '';
+$config['language'] = 'english';
+$config['charset'] = 'UTF-8';
+$config['enable_hooks'] = TRUE;
+$config['subclass_prefix'] = 'MY_';
+$config['composer_autoload'] = '../vendor/autoload.php';
+$config['permitted_uri_chars'] = 'a-z 0-9~%.:_\-';
+$config['enable_query_strings'] = FALSE;
+$config['controller_trigger'] = 'c';
+$config['function_trigger'] = 'm';
+$config['directory_trigger'] = 'd';
+$config['allow_get_array'] = TRUE;
+$config['log_threshold'] = 4; // UBAH KE 4 UNTUK SEMUA LOG
+$config['log_path'] = '';
+$config['log_file_extension'] = '.log';
+$config['log_file_permissions'] = 0644;
+$config['log_date_format'] = 'Y-m-d H:i:s';
+$config['error_views_path'] = '';
+$config['cache_path'] = '';
+$config['cache_query_string'] = FALSE;
 
-$config['cookie_prefix']              = '';
-$config['cookie_domain']              = '';
-$config['cookie_path']                  = '/';
-$config['cookie_secure']              = FALSE;
-$config['cookie_httponly']             = TRUE;
-$config['standardize_newlines']     = FALSE;
-$config['global_xss_filtering']     = FALSE;
+$config['encryption_key'] = 'wpAc3HuWyJ50BLK0YqeQ4X9j7tOkIC8Nla7J4xVG';
 
-$config['csrf_protection']          = FALSE;
-$config['csrf_token_name']          = 'csrf_test_name';
-$config['csrf_cookie_name']         = 'csrf_cookie_name';
-$config['csrf_expire']              = 7200;
-$config['csrf_regenerate']          = TRUE;
-$config['csrf_exclude_uris']        = array();
+$config['standardize_newlines'] = FALSE;
+$config['global_xss_filtering'] = FALSE;
 
-$config['compress_output']                 = FALSE;
-$config['time_reference']                 = 'local';
-$config['rewrite_short_tags']           = FALSE;
-$config['proxy_ips']                         = '';
+$config['compress_output'] = FALSE;
+$config['time_reference'] = 'local';
+$config['rewrite_short_tags'] = FALSE;
+$config['proxy_ips'] = '';
 
-// **SESSION CONFIGURATION FIX FOR POSTGRESQL**
-$config['sess_driver'] = 'database';
+// **GUNAKAN FILES SESSION DULU - LEBIH STABIL**
+$config['sess_driver'] = 'files';
 $config['sess_cookie_name'] = 'ci_session';
 $config['sess_expiration'] = 7200;
-$config['sess_save_path'] = 'ci_sessions';
+$config['sess_save_path'] = '/tmp';
 $config['sess_match_ip'] = FALSE;
 $config['sess_time_to_update'] = 300;
 $config['sess_regenerate_destroy'] = FALSE;
-
-// **CRITICAL: TURN OFF ENCRYPTION FOR POSTGRESQL**
 $config['sess_encrypt_cookie'] = FALSE;
 
 $config['cookie_prefix'] = '';
@@ -74,20 +57,9 @@ $config['cookie_path'] = '/';
 $config['cookie_secure'] = FALSE;
 $config['cookie_httponly'] = FALSE;
 
-$config['standardize_newlines'] = FALSE;
-$config['global_xss_filtering'] = FALSE;
-
 $config['csrf_protection'] = FALSE;
 $config['csrf_token_name'] = 'csrf_test_name';
 $config['csrf_cookie_name'] = 'csrf_cookie_name';
 $config['csrf_expire'] = 7200;
 $config['csrf_regenerate'] = TRUE;
 $config['csrf_exclude_uris'] = array();
-
-$config['compress_output'] = FALSE;
-$config['time_reference'] = 'local';
-$config['rewrite_short_tags'] = FALSE;
-$config['proxy_ips'] = '';
-
-// Encryption key
-$config['encryption_key'] = 'wpAc3HuWyJ50BLK0YqeQ4X9j7tOkIC8Nla7J4xVG';
